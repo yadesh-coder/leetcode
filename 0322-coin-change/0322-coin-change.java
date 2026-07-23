439602ebf4e67d9f33f39db20f2df52a07d22533
yadesh <yadesh929@gmail.com>
2026-07-23 15:40:33 +0530

    Time: 22 ms (34.43%), Space: 47.3 MB (15.99%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,19 +1,23 @@
 class Solution {
-public int coinChange(int[] coins, int amount) {
-    int[] dp = new int[amount+1];
-    Arrays.fill(dp,-1);
-    dp[0]=0;
-    for (int coin:coins) {
-        for (int j=1;j<=amount;j++){
-            if (j>=coin&&dp[j-coin]!=-1){
-                if (dp[j]==-1) {
-                   dp[j]=1+dp[j-coin];
-                } else {
-                    dp[j]=Math.min(dp[j],1+dp[j-coin]);
+    public int coinChange(int[] coins, int amount) {
+        int m=coins.length;
+        int[][] dp=new int[m+1][amount+1];
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=amount;j++){
+                if(j==0){
+                    dp[i][j]=0;
                 }
-              }
-         }
-      }
-    return dp[amount];
+                else if(i==0){
+                    dp[i][j]=Integer.MAX_VALUE-1;
+                }
+                else if(coins[i-1]>j){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=Math.min(dp[i-1][j],1+dp[i][(j-coins[i-1]) ]);
+                }
+            }
+        }
+        return dp[m][amount] >= 100000 ? -1 : dp[m][amount];
     }
 }
