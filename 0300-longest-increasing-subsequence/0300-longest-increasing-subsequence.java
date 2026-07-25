5504595376c4ff1f2a69a7dc19f1a9c96bc46fd2
yadesh <yadesh929@gmail.com>
2026-07-25 10:04:12 +0530

    Time: 5 ms (87.64%), Space: 46.4 MB (32.69%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,28 +1,26 @@
 class Solution {
     private int lowerBound(List<Integer> arr, int target) {
-        int low = 0;
-        int high = arr.size() - 1;
-        int ans = arr.size(); 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr.get(mid) >= target) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+        int low=0;
+        int high=arr.size()-1;
+        int ans=arr.size(); 
+        while (low<=high) {
+            int mid=low+(high-low)/2;
+            if (arr.get(mid)>=target){
+                ans=mid;
+                high=mid-1;
+            } else 
+                low=mid+1;
         }
         return ans;
     }
-    public int lengthOfLIS(int[] nums) {
-        List<Integer> tails = new ArrayList<>();
-        for (int num : nums) {
-            int pos = lowerBound(tails, num);
-            if (pos == tails.size()) {
+    public int lengthOfLIS(int[] nums){
+        List<Integer> tails=new ArrayList<>();
+        for(int num:nums){
+            int pos=lowerBound(tails,num);
+            if(pos==tails.size()){
                 tails.add(num); 
-            } else {
-                tails.set(pos, num); 
-            }
+            } else
+                tails.set(pos,num); 
         }
         return tails.size();
     }
