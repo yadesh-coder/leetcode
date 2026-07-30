3fc776f197c73a0d202c3aa73c1eaa1b2d1e5085
yadesh <yadesh929@gmail.com>
2026-07-30 15:33:34 +0530

    Time: 23 ms (28.8%), Space: 59.4 MB (62.89%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,13 @@
+class RecentCounter {
+    Queue<Integer> q;
+    public RecentCounter() {
+        q=new LinkedList<>();
+    }
+    public int ping(int t) {
+        q.add(t);
+        while(!q.isEmpty()&& t-3000>q.peek()){
+            q.poll();
+        }
+        return q.size();   
+    }
+}
