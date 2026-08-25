5dbaea6dbe3d9ebd938ead30b6e983fc415597a1
Yadesh M <yadesh929@gmail.com>
2026-08-25 09:51:04 +0530

    Time: 8 ms (4.71%), Space: 119.8 MB (10.3%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        if (Arrays.stream(gas).sum() < Arrays.stream(cost).sum()) {
+            return -1;
+        }
+        int tank = 0, idx = 0;
+        for (int i = 0; i < gas.length; i++) {
+            tank += gas[i] - cost[i];
+            if (tank < 0) {
+                tank = 0;
+                idx = i + 1;
+            }
+        }
+        return idx;
+    }
+}
