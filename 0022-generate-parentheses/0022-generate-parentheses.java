d9774c1e45731e0c26b8f61421976cc260fcdbe8
Yadesh M <yadesh929@gmail.com>
2026-09-11 09:14:38 +0530

    Time: 2 ms (69.41%), Space: 44.8 MB (32.27%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -10,7 +10,7 @@ class Solution {
             return;
         }
         if (openP < n) {
-            dfs(openP + 1, closeP, s + "(", n, res);
+        dfs(openP + 1, closeP, s + "(", n, res);
         }
         if (closeP < openP) {
             dfs(openP, closeP + 1, s + ")", n, res);
